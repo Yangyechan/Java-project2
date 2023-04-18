30e52a8c53f7c76a4fd18efe7e92a317815e85d4
yechanYang <yac392045@mokpo.ac.kr>
2023-04-18 18:59:46 +0900

    first commit

---
+++ src/main/java/com/example/project1/PrintBye.java
@@ -0,0 +1,7 @@
+package com.example.project1;
+
+public class PrintBye {
+    public void print(){
+        System.out.println("Bye");
+    }
+}
---
+++ src/main/java/com/example/project1/PrintHello.java
@@ -0,0 +1,7 @@
+package com.example.project1;
+
+public class PrintHello {
+    public void print () {
+        System.out.println(1 + 1);
+    }
+}
