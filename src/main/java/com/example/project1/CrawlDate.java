bf5e1099b271c3748d1839abe87ba16ca9d72e4e
yechanYang <yac392045@mokpo.ac.kr>
2023-04-19 13:24:40 +0900

    feat: 마지막 크롤시간 작업1

---
+++ src/main/java/com/example/project1/CrawlDate.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+    private LocalDateTime lastCrawlDateTime;
+    // alt + insert
+
+    public CrawlDate(LocalDateTime lastCrawlDateTime) {
+        this.lastCrawlDateTime = lastCrawlDateTime;
+    }
+}
+
